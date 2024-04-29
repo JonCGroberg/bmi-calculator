4b2d3c050684ade958c3c85c5936601f3902390b
Jonathan Groberg <Jgroberg@teamhcso.com>
2024-04-29 15:06:26 -0400

    theme

--- bmiCalculator.js
+++ bmiCalculator.js
@@ -1,26 +1,26 @@
-// window.onload = () => {
-// 	// grab the id for the needed elements
-// 	// input elements
-// 	const weightInput = document.getElementById("weight");
-// 	const inchesInput = document.getElementById("inches");
-// 	const feetInput = document.getElementById("feet");
-// 	// form element
-// 	const bmiForm = document.getElementById("bmiForm");
-// 	// label element (for showing the user the result)
-// 	const calculatedBMILabel = document.getElementById("calculatedBMI");
+window.onload = () => {
+	// grab the id for the needed elements
+	// input elements
+	const weightInput = document.getElementById("weight");
+	const inchesInput = document.getElementById("inches");
+	const feetInput = document.getElementById("feet");
+	// form element
+	const bmiForm = document.getElementById("bmiForm");
+	// label element (for showing the user the result)
+	const calculatedBMILabel = document.getElementById("calculatedBMI");
 
-// 	function caluculateBMI({ weight, height }) {
-// 		return (weight / Math.pow(height, 2)) * 703;
-// 	}
+	function caluculateBMI({ weight, height }) {
+		return (weight / Math.pow(height, 2)) * 703;
+	}
 
-// 	bmiForm.onsubmit = (e) => {
-// 		e.preventDefault();
+	bmiForm.onsubmit = (e) => {
+		e.preventDefault();
 
-// 		data = {
-// 			weight: Number(weightInput.value),
-// 			height: Number(inchesInput.value) + Number(feetInput.value) * 12
-// 		};
+		data = {
+			weight: Number(weightInput.value),
+			height: Number(inchesInput.value) + Number(feetInput.value) * 12
+		};
 
-// 		calculatedBMILabel.innerText = caluculateBMI(data);
-// 	};
-// };
+		calculatedBMILabel.innerText = caluculateBMI(data).toFixed(2);
+	};
+};
