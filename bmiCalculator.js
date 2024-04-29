44c80f91635fe68c115a5da2d23813687666a41d
Jonathan <jongroberg@gmail.com>
2024-04-29 11:40:30 -0400

    init commit

---
+++ bmiCalculator.js
@@ -0,0 +1,26 @@
+// window.onload = () => {
+// 	// grab the id for the needed elements
+// 	// input elements
+// 	const weightInput = document.getElementById("weight");
+// 	const inchesInput = document.getElementById("inches");
+// 	const feetInput = document.getElementById("feet");
+// 	// form element
+// 	const bmiForm = document.getElementById("bmiForm");
+// 	// label element (for showing the user the result)
+// 	const calculatedBMILabel = document.getElementById("calculatedBMI");
+
+// 	function caluculateBMI({ weight, height }) {
+// 		return (weight / Math.pow(height, 2)) * 703;
+// 	}
+
+// 	bmiForm.onsubmit = (e) => {
+// 		e.preventDefault();
+
+// 		data = {
+// 			weight: Number(weightInput.value),
+// 			height: Number(inchesInput.value) + Number(feetInput.value) * 12
+// 		};
+
+// 		calculatedBMILabel.innerText = caluculateBMI(data);
+// 	};
+// };
---
+++ toolcode.php
@@ -0,0 +1,71 @@
+<?php
+?>
+
+<!-- Formula: weight (lb) / [height (in)]2 x 703 -->
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Calculator</title>
+    <script src="./bmiCalculator.js"></script>
+</head>
+
+<body>
+    <form id="bmiForm">
+        <div>
+            <label for="weight">Weight (lbs)</label>
+            <input id="weight" name="weight" type="text" />
+        </div>
+        <br />
+        <label for=""> Height (in)</label>
+        <div>
+            <label for="feet">Feet</label>
+            <input id="feet" name="feet" type="text" />
+        </div>
+        <div>
+            <label for="inches">Inches</label>
+            <input id="inches" name="inches" type="text" />
+        </div>
+        <br />
+        <button>Calculate</button>
+        <div>
+            <br />
+            <label for="">BMI: <span id="calculatedBMI"></span></label>
+        </div>
+    </form>
+    <br />
+
+</body>
+
+<script language="javascript">
+    window.onload = () => {
+        // grab the id for the needed elements
+        // input elements
+        const weightInput = document.getElementById("weight");
+        const inchesInput = document.getElementById("inches");
+        const feetInput = document.getElementById("feet");
+        // form element
+        const bmiForm = document.getElementById("bmiForm");
+        // label element (for showing the user the result)
+        const calculatedBMILabel = document.getElementById("calculatedBMI");
+
+        function caluculateBMI({ weight, height }) {
+            return (weight / Math.pow(height, 2)) * 703;
+        }
+
+        bmiForm.onsubmit = (e) => {
+            e.preventDefault();
+
+            data = {
+                weight: Number(weightInput.value),
+                height: Number(inchesInput.value) + Number(feetInput.value) * 12
+            };
+
+            calculatedBMILabel.innerText = caluculateBMI(data);
+        };
+    };
+</script>
+<html>
